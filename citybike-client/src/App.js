f2e3cfbe359dd8439ea5af80e83ed9186eee6fc8
fpieschaconr <fpieschaconr@unal.edu.co>
2021-08-18 12:11:14 -0500

    :construction: front end recieves stations

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -9,22 +9,25 @@ class App extends Component {
     this.state = {
       response: false,
       endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      lat: 25.808681,
+      lng: -80.191788,
+      zoom: 12,
     };
-
   }
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-   
+    socket.emit("ready");
+    socket.on("getMiamiAvailability", (data) => {
+      this.setState({ response: data });
+      console.log(data.network.stations);
+    });
   }
   render() {
     const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
+    const stations = response ? response.network.stations : [];
+    const position = [this.state.lat, this.state.lng];
     return (
-
       <div className="map">
         <h1> City Bikes in Miami </h1>
         <Map center={position} zoom={this.state.zoom}>
@@ -32,6 +35,25 @@ class App extends Component {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          {stations.map(
+            ({
+              id,
+              empty_slots,
+              free_bikes,
+              latitude,
+              longitude,
+              name,
+              extra,
+            }) => (
+              <Marker position={[latitude, longitude]} key={id}>
+                <Popup>
+                  {name} <br />
+                  Free Bikes: {free_bikes} <br />
+                  Empty Slots: {empty_slots}
+                </Popup>
+              </Marker>
+            )
+          )}
         </Map>
       </div>
     );
