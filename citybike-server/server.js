a005dd5b718ee7d4f478c5af1c95180cab7b7a52
fpieschaconr <fpieschaconr@unal.edu.co>
2021-08-18 12:10:45 -0500

    :sparkles: backend sends stations every 5 secs

--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
-
-
+const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach";
+const axios = require("axios");
+const cron = require("node-cron");
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
 
+const getData = async () => {
+  return await axios.get(citybikeurl);
+};
 
 app.use(index);
 
@@ -16,18 +19,24 @@ const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
 let interval;
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
-  console.log('New connection ' + socketId + ' from ' + clientIp);
+  console.log("New connection " + socketId + " from " + clientIp);
+  socket.on("ready", function () {
+    getData()
+      .then((res) => io.emit("getMiamiAvailability", res.data))
+      .catch((err) => console.error(err));
+  });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
-
+cron.schedule("*/5 * * * * *", function () {
+  getData()
+    .then((res) => io.emit("getMiamiAvailability", res.data))
+    .catch((err) => console.error(err));
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
